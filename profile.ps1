1315de8403d4f1a28282b43fa51c61ce826a8956
Radovan Monček <radovan.moncek1@gmail.com>
2025-10-26 11:43:30 +0100

    refactor: modify fzf / Fzf binding, and restructure

--- profile.ps1
+++ profile.ps1
@@ -1,35 +1,14 @@
 $work = "C:\Users\soudr\work"
 $school = "$($HOME)\school"
 $personal = "$($HOME)\personal\git"
+$env:FZF_DEFAULT_OPTS = '--multi --height=69% --margin=5%,2%,2%,5% --layout=reverse-list --border=double --info=inline --prompt="./' + ($pwd -split "\\")[($pwd -split "\\").Length - 1] + ' ? " --pointer="@" --marker="x" --header="CTRL-c or ESC to quit" --color="dark,fg+:cyan,pointer:cyan,fg:yellow"'
 
-#Import-Module -Name Terminal-Icons
-
-# Steam 
-New-Alias steam "C:\Program Files (x86)\Steam\steam.exe"
-# Opera
-New-Alias opera "C:\Users\Admin\AppData\Local\Programs\Opera GX\opera.exe"
-# League
-New-Alias league "C:\Users\Admin\Documents\gameLaunch\league.lnk"
-#cmatrix
-New-Alias cmatrix Start-ScreenSaver
-
-#neofetch custom
 Function setDukeAsArt {neofetch --source C:\Users\Admin\scoop\apps\neofetch\neofetch_duke_ascii.txt}
 
-#neo
-function clearAndFetch{
+Function clearAndFetch{
 	clear
 	setDukeAsArt
 }
-
-New-Alias neo clearAndFetch
-#Code
-New-Alias coder "C:\Users\Admin\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-#firefox
-New-Alias fox "C:\Program Files\Mozilla Firefox\firefox.exe"
-New-Alias neoduke setDukeAsArt
-New-Alias dockerdesktop "C:\Program Files\Docker\Docker\frontend\Docker Desktop.exe"
-New-Alias idea64 C:\Portable\idea24\app\bin\idea64.exe
 
 # Inspired by Gruber Darker - tsoding
 Function prompt {
@@ -46,7 +25,6 @@ Function Path-From-NTFS-To-Ext4 ([string] $FileNameNTFS){
 }
 
 Function Vim-WSL ([string]$FileNameExt4) {
-	#echo $FileNameExt4
 	wsl vim (Path-From-NTFS-To-Ext4 $FileNameExt4)
 }
 
@@ -58,17 +36,11 @@ Function Get-Message-Of-The-Day {
 	clear; Write-Host "Those who do not learn from history are destined to repeat it. (George Saytama)"
 }
 
+#https://www.windowsdigitals.com/powershell-open-url-in-chrome-edge-or-firefox/
 Function FZF-Into-Vim {
-	#Write-Host "You just launched vim fzf"
-	#$NTFSPath = (fzf)
-	#echo $NTFSPath
-	#fzf | echo
-	#Get-ChildItem . -File -Recurse -Depth 4 | Invoke-Fzf | % {type $_}
-	#prompt
 	[Microsoft.PowerShell.PSConsoleReadLine]::BackwardKillLine()
 	Get-ChildItem . -File -Recurse -Depth 4 | Invoke-Fzf | % {Vim-WSL $_}
-	#return
-	#exit
+	#[System.Diagnostics.Process]::Start("firefox", "https://www.windowsdigitals.com")
 }
 
 Function File-WSL ([string]$NTFSPath) {
@@ -91,39 +63,21 @@ New-Alias dis Start-Discord
 New-Alias discord dis
 New-Alias zero "C:\ProgramData\ZeroTier\One\zerotier-one_x64.exe"
 New-Alias zerotier-cli zero
+New-Alias steam "C:\Program Files (x86)\Steam\steam.exe"
+New-Alias opera "C:\Users\Admin\AppData\Local\Programs\Opera GX\opera.exe"
+New-Alias league "C:\Users\Admin\Documents\gameLaunch\league.lnk"
+New-Alias cmatrix Start-ScreenSaver
+New-Alias neo clearAndFetch
+New-Alias coder "C:\Users\Admin\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+New-Alias fox "C:\Program Files\Mozilla Firefox\firefox.exe"
+New-Alias neoduke setDukeAsArt
+New-Alias dockerdesktop "C:\Program Files\Docker\Docker\frontend\Docker Desktop.exe"
+New-Alias idea64 C:\Portable\idea24\app\bin\idea64.exe
 
-Set-PSReadLineKeyHandler -Chord 'Ctrl+u' -ScriptBlock {
-#echo 1
-#[Microsoft.PowerShell.PSConsoleReadLine]::AddLine()
-#[Microsoft.PowerShell.PSConsoleReadLine]::Insert('echo 1')
+Set-PSReadLineKeyHandler -Chord 'Ctrl+o' -ScriptBlock {
 	[Microsoft.PowerShell.PSConsoleReadLine]::Insert('FZF-Into-Vim')
-	#[Microsoft.PowerShell.PSConsoleReadLine]::BackwardKillLine()
-	#FZF-Into-Vim
 	[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 	[Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-	#[Microsoft.PowerShell.PSConsoleReadLine]::Insert('')
-	#[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-#FZF-Into-Vim
-#return
-#exit
-#fzf | echo
-#Get-ChildItem . -File -Recurse | Invoke-Fzf | % {Write-Host "\n"; Get-Content -Path $_ | Write-Host; prompt}
-#Get-ChildItem . -File -Recurse | Invoke-Fzf | Vim-WSL
-#return
-#Invoke-Command -ScriptBlock {Write-Host "test"}
-#echo test
 } 
-
-Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -ScriptBlock {
-
-}
-
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-#Set-PsFzfOption -AltCCommand [ScriptBlock]{ 
-#param($Location) 
-#echo $Location 
-#[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine() 
-#}
-$env:FZF_DEFAULT_OPTS = '--multi --height=69% --margin=5%,2%,2%,5% --layout=reverse-list --border=double --info=inline --prompt="./' + ($pwd -split "\\")[($pwd -split "\\").Length - 1] + ' ? " --pointer="@" --marker="x" --header="CTRL-c or ESC to quit" --color="dark,fg+:cyan,pointer:cyan,fg:yellow"'
 
 Get-Message-Of-The-Day
